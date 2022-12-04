d55785329a9421c654c5e0ceaab28f26996d160d
anth7310 <anthonyin98@gmail.com>
2022-12-04 12:40:48 -0500

    completed day 1 part 2

--- 2022/1/main.py
+++ 2022/1/main.py
@@ -1,3 +1,4 @@
+## Part 1
 max_val = 0
 with open('input', 'r') as f:
     curr_val = 0
@@ -9,3 +10,22 @@ with open('input', 'r') as f:
         else:
             curr_val += int(num)
 print(max(max_val, curr_val))
+
+## Part 2
+import heapq
+h = []
+with open('input', 'r') as f:
+    curr_val = 0
+    lst = f.read().split('\n')
+    for num in lst:
+        if num == '':
+            heapq.heappush(h, curr_val)
+            if len(h) > 3:
+                heapq.heappop(h)
+            curr_val = 0
+        else:
+            curr_val += int(num)
+heapq.heappush(h, curr_val)
+if len(h) > 3:
+    heapq.heappop(h)
+print(sum(h))
