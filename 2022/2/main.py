52688c3f279971adbc09c385f5ca046fdd56ead8
anth7310 <anthonyin98@gmail.com>
2022-12-05 13:25:00 -0500

    finished part 2

--- 2022/2/main.py
+++ 2022/2/main.py
@@ -1,3 +1,4 @@
+# part 1
 with open('input', 'r') as f:
     points = 0
     score = {
@@ -12,4 +13,20 @@ with open('input', 'r') as f:
             points += 3
         elif a == 'A' and b == 'Y' or a == 'B' and b == 'Z' or a == 'C' and b == 'X':
             points += 6
+    print(points)
+
+# part 2
+with open('input', 'r') as f:
+    points = 0
+    for row in f.read().split('\n'):
+        a, b = row.split()
+        condition = {
+            # lose
+            'X': {'A': 3, 'B': 1, 'C': 2}, 
+            # tie
+            'Y': {'A': 1 + 3, 'B': 2 + 3, 'C': 3 + 3},
+            # win
+            'Z': {'A': 2 + 6, 'B': 3 + 6, 'C': 1 + 6},
+        }
+        points += condition[b][a]
     print(points)
